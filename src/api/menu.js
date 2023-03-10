825b56c7df750d9a6047db1b75f774ef4de16721
MatsuSakura <lujiahao0994@gmail.com>
2023-03-10 22:21:08 +0800

    sysMenu

---
+++ src/api/menu.js
@@ -0,0 +1,15 @@
+import http from '@/utils/http'
+//新增菜单
+export async function addMenuApi(parm){
+    return await http.post("/api/menu",parm);
+}
+
+//获取菜单列表
+export async function getMenuListApi(){
+    return await http.get("/api/menu/list",null);
+}
+
+//获取上级部门
+export async function getParentApi(){
+    return await http.get("/api/menu/parent")
+}
