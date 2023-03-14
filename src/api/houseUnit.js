e6ac864d06524003c3e48256c3c2454185fa2a9c
MatsuSakura <lujiahao0994@gmail.com>
2023-03-14 14:13:22 +0800

    单元增加页面

--- src/api/houseUnit.js
+++ src/api/houseUnit.js
@@ -3,3 +3,19 @@ import http from '@/utils/http'
 export async function getListApi(parm){
     return http.get("/api/houseUnit/list",parm)
 }
+//获取栋数列表
+export async function getBuildListApi(parm){
+    return await http.get("/api/houseBuild/list",parm);
+}
+//新增
+export async function addApi(parm){
+    return await http.post("/api/houseUnit",parm)
+}
+//编辑
+export async function editApi(parm){
+    return await http.put("/api/houseUnit",parm)
+}
+//删除
+export async function deleteApi(parm){
+    return await http.delete("/api/houseUnit",parm)
+}
--- src/views/house/houseUnit.vue
+++ src/views/house/houseUnit.vue
@@ -47,15 +47,89 @@
         layout="total, sizes, prev, pager, next, jumper"
         :total="parms.total" background>
       </el-pagination>
-      
+      <!-- 新增或编辑弹框 -->
+    <sys-dialog
+      :title="addDialog.title"
+      :height="addDialog.height"
+      :width="addDialog.width"
+      :visible="addDialog.visible"
+      @onClose="onClose"
+      @onConfirm="onConfirm"
+    >
+      <template slot="content">
+        <el-form
+          :model="addModel"
+          ref="addForm"
+          :rules="rules"
+          label-width="80px"
+          :inline="true"
+          size="small"
+        >
+          <el-form-item prop="buildId" label="单元名称">
+            <el-select v-model="addModel.buildId">
+              <el-option
+                v-for="item in bulidTabeList"
+                :key="item.buildId"
+                :label="item.name"
+                :value="item.buildId"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item prop="unitName" label="单元名称">
+            <el-input v-model="addModel.unitName"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+    </sys-dialog>
     </el-main>
   </template>
   
   <script>
-  import { getListApi } from "@/api/houseUnit";
+  import { getListApi,getBuildListApi,addApi } from "@/api/houseUnit";
+  import SysDialog from "@/components/system/SysDialog.vue";
   export default {
+    components: {
+    SysDialog,
+  },
     data() {
       return {
+        rules: {
+        buildId: [
+          {
+            trigger: "change",
+            required: true,
+            message: "请选择栋数",
+          },
+        ],
+        unitName: [
+          {
+            trigger: "change",
+            required: true,
+            message: "请填写单元名称",
+          },
+        ],
+      },
+      addModel: {
+        editType: "",
+        unitId: "",
+        buildId: "",
+        unitName: "",
+      },
+      bulidTabeList: [],
+      //栋数查询参数
+      buildParm: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0,
+      },
+      //弹框属性
+      addDialog: {
+        title: "",
+        height: 150,
+        width: 620,
+        visible: false,
+      },
         //表格高度
         tableHeight:0,
         //表格数据
@@ -72,6 +146,7 @@
     },
     created() {
       this.getList();
+      this.getBuildList();
     },
     mounted(){
       this.$nextTick(()=>{
@@ -79,6 +154,27 @@
       })
     },
     methods: {
+        //新增或编辑确认事件
+    onConfirm() {
+      this.$refs.addForm.validate(async(valid) => {
+        if (valid) {
+          let res = null;
+          if(this.addModel.editType == '0'){
+            res = await addApi(this.addModel);
+          }
+          if(res && res.code == 200){
+            //刷新表格
+            this.getList();
+            this.$message.success(res.msg)
+             this.addDialog.visible = false;
+          }
+        }
+      });
+    },
+    //新增或编辑关闭
+    onClose() {
+      this.addDialog.visible = false;
+    },
       //页数改变时触发
       currentChange(val){
   
@@ -96,7 +192,15 @@
   
       },
       //新增
-      addBtn() {},
+      addBtn() {
+      //清空表单
+      this.$resetForm("addForm", this.addModel);
+      this.addModel.editType = "0";
+      //设置弹框属性
+      this.addDialog.visible = true;
+      this.addDialog.title = "新增单元";
+      //
+    },
       //重置
       resetBtn() {
         this.parms.buildNme = '';
@@ -116,6 +220,15 @@
         }
         console.log(res);
       },
+      //获取栋数列表
+    async getBuildList() {
+      let res = await getBuildListApi(this.buildParm);
+      console.log(res);
+      if (res && res.code == 200) {
+        this.bulidTabeList = res.data.records;
+        this.buildParm.total = res.data.total;
+      }
+    },
     },
   };
   </script>
