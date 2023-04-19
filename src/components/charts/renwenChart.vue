2c660ae139569e0def85c1793aa6dbae85606fac
MatsuSakura <lujiahao0994@gmail.com>
2023-04-20 00:52:52 +0800

    charts01

---
+++ src/components/charts/renwenChart.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <div id="main" style="width: 1000px;height:400px;"></div>
+    </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import SysDialog from "@/components/system/SysDialog.vue";
+import { getListApi as getComList } from "@/api/userComplaint"
+export default {
+    components: {
+        SysDialog,
+    },
+
+    data() {
+        this.myChart = null
+        return {
+            parms: {
+                currentPage: 1,
+                pageSize: 10,
+                title: "",
+                complaintContent: "",
+                total: 0,
+                status: "",
+                isHelp: "",
+                offerId: "",
+                offerGroup: ""
+            },
+            option: {
+                title: {
+                    text: '人文关怀事件的年龄段分析'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['年龄']
+                },
+                xAxis: {
+                    data: []
+                },
+                yAxis: [{
+                    type: 'value',
+                    name: '年龄段',
+                }],
+                series: [
+                    {
+                        name: '年龄段',
+                        type: 'line',
+                        data: []
+                    }
+                ]
+            },
+        };
+
+    },
+    created() {
+        this.$nextTick(() => {
+            // this.getData()
+        });
+
+    },
+    methods: {
+        getData() {
+            getComList(this.parms).then((res) => {
+                for (var i = 0; i < res.data.records.length; i++) {
+                    console.log(res.data.records);
+                    var item = res.data.records[i];
+                    console.log(item.complaintContent);
+                    this.option.xAxis.data.push(item.complaintContent);
+                    this.option.series[0].data.push(item.offerId);
+                }
+            })
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+        this.myChart = echarts.init(document.getElementById('main'));
+        this.getData();
+        setTimeout(() => {
+            this.myChart.setOption(this.option);
+        }, 500)
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,61 +1,75 @@
 <template>
-  
-  <div class="dashboard-container">
-    <el-table :height="tableHeight" :data="tableList" border stripe>
+  <el-main>
+    <div class="dashboard-container">
+      <el-table :height="tableHeight" :data="tableList" border stripe>
         <el-table-column label="标题" prop="title"></el-table-column>
         <el-table-column label="内容" prop="noticeContent"></el-table-column>
         <el-table-column label="时间" prop="createTime"></el-table-column>
-  </el-table>
-    <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80">
-    <div class="dashboard-text">name: {{ name }}<br>
-  </div><br>
-
-  
-
-    <div id="main" style="width: 600px;height:400px;"></div>
-  </div>
+      </el-table>
+      <!-- <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"> -->
+      <div class="dashboard-text">name: {{ name }}<br></div>
+      <el-button @click="rwBtn" type="success" plain>人文关怀事件的年龄段分析</el-button>
+    </div>
+    <rw-chart v-if="parms.visible != false"/>
+  </el-main>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import * as echarts from 'echarts'
-
-import { getListApi} from "@/api/notice";
-  import SysDialog from "@/components/system/SysDialog.vue";
-  export default {
-    components: {
-      SysDialog,
-    },
-    data() {
-      return {
-        //表格高度
-        tableHeight: 0,
-        //表格数据
-        tableList: [],
-        parms: {
-          currentPage: 1,
-          pageSize: 10,
-          title: "",
-          total: 0,
-        },
-      };
-    },
-    created() {
-      this.getlList();
-    },
-    methods: {
-      //获取列表
-      async getlList() {
-        let res = await getListApi(this.parms);
-        if (res && res.code == 200) {
-          //给表格赋值
-          console.log(res);
-          this.tableList = res.data.records;
-          this.parms.total = res.data.total;
-        }
+import { getListApi } from "@/api/notice";
+import SysDialog from "@/components/system/SysDialog.vue";
+import rwChart from "@/components/charts/renwenChart.vue";
+export default {
+  components: {
+    SysDialog,
+    rwChart
+  },
+  data() {
+    return {
+      count: [],
+      //表格高度
+      tableHeight: 0,
+      //表格数据
+      tableList: [],
+      addModel:{
+        visible:false
       },
+      parms: {
+        currentPage: 1,
+        pageSize: 10,
+        title: "",
+        complaintContent: "",
+        total: 0,
+        status: "",
+        visible:false,
+        isHelp: "",
+        offerId: "",
+        offerGroup: ""
+      },
+    };
+  },
+  created() {
+    this.$nextTick(() => {
+      this.getlList();
+    });
+  },
+  methods: {
+    rwBtn(){
+      console.log("确定");
+      this.parms.visible = !this.parms.visible
     },
-      
+    //获取列表
+    async getlList() {
+      let res = await getListApi(this.parms);
+      if (res && res.code == 200) {
+        //给表格赋值
+        console.log(res);
+        this.tableList = res.data.records;
+        this.parms.total = res.data.total;
+      }
+    },
+  },
+
   name: 'Dashboard',
   computed: {
     ...mapGetters([
@@ -64,34 +78,8 @@ import { getListApi} from "@/api/notice";
   },
   mounted() {
     this.$nextTick(() => {
-        this.tableHeight = 100;
-      });
-    var myChart = echarts.init(document.getElementById('main'));
-
-      // 指定图表的配置项和数据
-      var option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [
-          {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      };
-
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
+      this.tableHeight = 100;
+    });
   },
 }
 </script>
@@ -101,6 +89,7 @@ import { getListApi} from "@/api/notice";
   &-container {
     margin: 30px;
   }
+
   &-text {
     font-size: 30px;
     line-height: 46px;
